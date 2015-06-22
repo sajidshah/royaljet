e429a67a8fde002673b001265abdfb483c4bb884
Sajid Shah <syedsajidshah@live.com>
2015-06-22 22:05:18 +0500

    update

--- wp-content/themes/runway-bootstrap-starter/email.php
+++ wp-content/themes/runway-bootstrap-starter/email.php
@@ -2,7 +2,7 @@
 /* Template Name: Email Test */
 
 $headers = 'From: My Name <myname@sajidshah.com>' . "\r\n\\";
-$test = mail('syedsajidshah@live.com', 'The subject', 'The message');
+$test = mail('syedsajidshah@live.com, dijast@gmail.com', 'The subject', 'The message');
 //$test = mail('caffeinated@example.com', 'My Subject', $message);
  
 if($test) echo "SUCCESS";
