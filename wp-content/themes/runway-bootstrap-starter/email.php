80c7ed46e9b4f353b1fa8afba84589bce96a91fc
Sajid Shah <syedsajidshah@live.com>
2015-06-22 21:57:06 +0500

    test

--- wp-content/themes/runway-bootstrap-starter/email.php
+++ wp-content/themes/runway-bootstrap-starter/email.php
@@ -1,9 +1,8 @@
 <?php
 /* Template Name: Email Test */
-get_header();
+
 $headers = 'From: My Name <myname@sajidshah.com>' . "\r\n\\";
 $test = wp_mail('syedsajidshah@live.com', 'The subject', 'The message',  $headers); 
 if($test) echo "SUCCESS";
 else echo "FAILED"; 
-
-get_footer(); ?>
+?>
