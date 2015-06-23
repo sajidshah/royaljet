209296b73b5ee43d6fbfb8d9feef0e306e07c801
Sajid Shah <syedsajidshah@live.com>
2015-06-24 04:36:21 +0500

    date format

--- wp-content/themes/runway-bootstrap-starter/content-jetlist.php
+++ wp-content/themes/runway-bootstrap-starter/content-jetlist.php
@@ -14,7 +14,12 @@
 				}
 			?> 
 			<h2><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></h2>
-			<strong>Date</strong>: <?php echo get_field( "available_date" ); ?><br>
+			<strong>Date</strong>: 
+				<?php $date = get_field( "available_date" );
+				
+				echo $show_date = DateTime::createFromFormat('d/m/Y', $date)->format('m/d/Y');
+				?>
+			<br>
 			<strong>Departure</strong>: <?php echo get_field( "departure" ); ?><br>
 			<strong>Destination</strong>: <?php echo get_field( "destination" ); ?><br>
 			<strong>Max Pax</strong>: <?php echo get_field( "max_pax" ); ?><br>
